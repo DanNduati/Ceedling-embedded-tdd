00913581d4f7406da89c441987168258e6049022
DanielNduati <dannduati2@gmail.com>
2022-03-10 10:21:46 +0300

    Add lights module

---
+++ src/lights.h
@@ -0,0 +1,5 @@
+#ifndef LIGHTS_H
+#define LIGHTS_H
+
+
+#endif // LIGHTS_H
---
+++ test/test_lights.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "lights.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_lights_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement lights");
+}
+
+#endif // TEST

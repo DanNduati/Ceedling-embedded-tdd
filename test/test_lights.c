4f7cca6561121bc82d0f78c9f676219dbfb9ffdd
DanielNduati <dannduati2@gmail.com>
2022-03-10 11:47:03 +0300

    Add Test
    Desired behaviour: When the headlight switch is off, then the headlights are off
    Here i define 2 new functions to be implementd in the lights module:
    - lights_SetHeadlightSwitchOff()//turns the lights off lol
    - lights_AreHeadlightsOn()//returns the state of the headlights
    I use the `TEST_ASSERT_EQUAL()` unity macro to verify the state of the
    headlights

--- test/test_lights.c
+++ test/test_lights.c
@@ -11,10 +11,17 @@ void setUp(void)
 void tearDown(void)
 {
 }
-
+/*
 void test_lights_NeedToImplement(void)
 {
     TEST_IGNORE_MESSAGE("Need to Implement lights");
 }
-
+*/
+void test_WhenTheHeadlightSwitchIsOff_ThenTheHeadlightsAreOff(void)
+{
+    // turn headlight switch off
+    lights_setHeadlightSwitchOff();
+    //confirm the the state of the headlights
+    TEST_ASSERT_EQUAL(false, lights_AreHeadlighsOn());
+}
 #endif // TEST

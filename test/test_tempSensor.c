a21d039ad50cebe3eba9bbcbb9b1dc06e9fb3991
DanielNduati <dannduati2@gmail.com>
2022-03-11 17:15:30 +0300

    Add first tempsensor test
    This test asserts that we get 104.6 as the temperature when we read the
    temperature register with a value of 0x3FF

--- test/test_tempSensor.c
+++ test/test_tempSensor.c
@@ -12,9 +12,17 @@ void tearDown(void)
 {
 }
 
-void test_tempSensor_NeedToImplement(void)
+void test_whenTempRegisterReadsMaxValue_thenTheTempIsTheMaxValue(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement tempSensor");
+    uint8_t tempRegisterAddress = 0x03;
+    float expectedTemperature = 104.6f;
+    float tolerance = 0.1f;
+    // When
+    i2c_readRegister_ExpectAndReturn(tempRegisterAddress, 0x3ff); // mock function
+    // Then
+    float actualTemperature = tempSensor_getTemperature();
+    TEST_ASSERT_FLOAT_WITHIN(tolerance, expectedTemperature,
+                             actualTemperature);
 }
 
 #endif // TEST

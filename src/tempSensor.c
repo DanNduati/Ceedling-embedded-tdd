ec2a4654b6cfb765ee0aad798a944d76bb15dd77
DanielNduati <dannduati2@gmail.com>
2022-03-13 11:59:04 +0300

    Implement `tempSensor_getTemperature` function
    Implement logic fot the function under test.

--- src/tempSensor.c
+++ src/tempSensor.c
@@ -1,5 +1,11 @@
+#include <stdint.h>
+#include <stdio.h>
 #include "tempSensor.h"
+#include "i2c.h"
 float tempSensor_getTemperature(void)
 {
-    return 0.0f;
+    uint16_t rawValue = i2c_readRegister(0x03);
+    float currentTemperature = -100.0f + (0.2f * (float)rawValue);
+    printf("rawvalue:%d temperature:%f", rawValue, currentTemperature);
+    return currentTemperature;
 }

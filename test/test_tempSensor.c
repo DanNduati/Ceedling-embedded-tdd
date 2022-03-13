b83eb5074bebd8553b707ac4b72f7114c8ecd96f
DanielNduati <dannduati2@gmail.com>
2022-03-13 11:24:15 +0300

    Mock the i2c interface
    - Create the header file i2c.h and within it declare the function
    prototype `i2c_readRegister()` function. Note that i dont actually
    implement this function however as declaring it is enough as we want
    ceedling to mock it with CMock.
    - Get ceedling mock this module is to add `#include "mock_i2c.h"` to
    `test_tempSensor.c`

--- test/test_tempSensor.c
+++ test/test_tempSensor.c
@@ -3,7 +3,7 @@
 #include "unity.h"
 
 #include "tempSensor.h"
-
+#include "mock_i2c.h"
 void setUp(void)
 {
 }
@@ -14,11 +14,12 @@ void tearDown(void)
 
 void test_whenTempRegisterReadsMaxValue_thenTheTempIsTheMaxValue(void)
 {
+
     uint8_t tempRegisterAddress = 0x03;
     float expectedTemperature = 104.6f;
     float tolerance = 0.1f;
-    // When
-    i2c_readRegister_ExpectAndReturn(tempRegisterAddress, 0x3ff); // mock function
+    // When you read a temperature register value of 0x3FF the temperature calculated should be 104.6
+    i2c_readRegister_ExpectAndReturn(tempRegisterAddress, 0x3ff); // mock function -> simulates/mocks the i2c module returning a value of 0x3ff on read of the temperature address
     // Then
     float actualTemperature = tempSensor_getTemperature();
     TEST_ASSERT_FLOAT_WITHIN(tolerance, expectedTemperature,
